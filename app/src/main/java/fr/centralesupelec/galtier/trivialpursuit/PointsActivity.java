752c04fa0f6868576451d11d1baf84a86a499eec
android <sabina.j.wang@gmail.com>
2025-11-13 09:42:35 +0100

    5 fini

--- app/src/main/java/fr/centralesupelec/galtier/trivialpursuit/PointsActivity.java
+++ app/src/main/java/fr/centralesupelec/galtier/trivialpursuit/PointsActivity.java
@@ -1,9 +1,14 @@
 package fr.centralesupelec.galtier.trivialpursuit;
 
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,36 +20,61 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class PointsActivity extends AppCompatActivity {
     final String CLE_NB_RECORD = "nbRecord";
+    final String CLE_NOM_RECORD = "nomRecord";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_points);
 
+        SharedPreferences sharedPreferences1 = getSharedPreferences("record", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor1 = sharedPreferences1.edit();
+
+        TextView tv = findViewById(R.id.textView);
+        TextView tvR = findViewById(R.id.textViewRecord);
         Intent appelant = getIntent();
         int points = appelant.getIntExtra("p",0);
         int totalPoints = appelant.getIntExtra("q", 0);
-        TextView tv = findViewById(R.id.textView);
+        Button bouton = findViewById(R.id.buttonEnregistrer);
+        EditText et = findViewById(R.id.editTextNom);
+        et.setVisibility(VISIBLE);
+        bouton.setVisibility(VISIBLE);
         float score = (float)points / totalPoints;
 
-        TextView textViewRecord = findViewById(R.id.textViewRecord);
-        EditText editTextNom = findViewById(R.id.editTextNom);
-        String name = editTextNom.getText().toString();;
+        String name = et.getText().toString();
 
-        SharedPreferences sharedPreferences1 = getSharedPreferences("record", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor1 = sharedPreferences1.edit();
         if (sharedPreferences1.contains(CLE_NB_RECORD)){
             float n = sharedPreferences1.getFloat(CLE_NB_RECORD, score);
-            textViewRecord.setText("Le record est : " +n);
-            editor1.putFloat(CLE_NB_RECORD, n);
-            editor1.apply();
+            String m = sharedPreferences1.getString(CLE_NOM_RECORD, name);
+            if (sharedPreferences1.getFloat(CLE_NB_RECORD, 0) < score) {
+                tvR.setText("Le précédent record (" + n + ") était détenu par " + m + ". Vous établissez le nouveau record !");
+                editor1.putFloat(CLE_NB_RECORD, score);
+                editor1.apply();
+
+            }
+            else {
+                et.setVisibility(INVISIBLE);
+                bouton.setVisibility(INVISIBLE);
+                tvR.setText("Ce n'est pas suffisant de prendre à " + m + " le titre de meilleur joueur avec un score de "+ n);
+            }
         }
         else{
             editor1.putFloat(CLE_NB_RECORD, score);
-            editor1.commit();
-            textViewRecord.setText("Vous etablissez le premier record !");
+            editor1.apply();
+            tvR.setText("Vous etablissez le premier record !");
         }
 
         tv.setText("Vous avez marqué "+ points +" points sur " + totalPoints+"\nScore: " + score);
+    }
+
+    public void enregistrer(View view) {
+
+        SharedPreferences sharedPreferences1 = getSharedPreferences("record", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor1 = sharedPreferences1.edit();
+        EditText et = findViewById(R.id.editTextNom);
+        String name = et.getText().toString();
+        editor1.putString(CLE_NOM_RECORD, name);
+        editor1.apply();
     }
 }
